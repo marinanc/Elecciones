954f8bc418180d2a8bc018c7b7591f81cb8c49d8
Marina Carrizo <marinancarrizo@gmail.com>
2020-09-11 19:10:42 -0300

    Fixed votar.js to validate DNI and estadoVoto
    Modified votar.js to validate if DNI is alredy in DataBase and check the value of estadoVoto
    - if DNI is already in DB and estadoVoto is false, go to seleccionarCandidato
    - if DNI is already in DB and estadoVoto is true, error message ("Ya se ha votado con el DNI ingresado")
    - if DNI is not in DB, register new "votante" and go to seleccionarCandidato
    
    if(Votante.find({ dni: dniVotante }, ...)) --> modified --> if(Votante.findOne({ dni: dniVotante }, ...))
    - With this modification findOne now can return null (find return "", not null)
    if (response != "") --> modified --> if (response != null && response.estadoVoto == true)

--- server/routes/votar.js
+++ server/routes/votar.js
@@ -18,55 +18,30 @@ router.post('/', function(req, res, next){
         }
     );
     var dniVotante = req.body.dni
-    if(Votante.find({ dni: dniVotante }, function(err, response){
+    if(Votante.findOne({ dni: dniVotante }, function(err, response){
         if (err) {
             res.render('votar', { title: 'Elecciones', error: err.message });
         }
-        if (response != "") {
-            res.render('votar', { title: 'Elecciones', error: 'Ya se ha votado con el DNI ingresado. ' + response});
+        if (response != null && response.estadoVoto == true) {
+            res.render('votar', { title: 'Elecciones', error: 'Ya se ha votado con el DNI ingresado. ' });
         }
         else {
-            votante.save(function (err, response) {
-                if (err) {
-                    return res.render('votar', { title: 'Elecciones' , error: err.message});
-                }
-                if (response == null){
-                    return res.render('votar', { title: Elecciones, error: "No se pudo registrar al votante..."})
-                }
+            if(response == null){
+                votante.save(function (err, response) {
+                    if (err) {
+                        return res.render('votar', { title: 'Elecciones' , error: err.message});
+                    }
+                    if (response == null){
+                        return res.render('votar', { title: Elecciones, error: "No se pudo registrar al votante..."})
+                    }
+                    res.redirect('seleccionarCandidato'); 
+                });
+            }
+            else {
                 res.redirect('seleccionarCandidato');
-            });
+            }
         }
     }));
-    // votante.save(function(err, response){
-    //     if (err){
-    //         return res.render('votar', { title: 'Elecciones' , error: err});
-    //     }
-    //     if (response == null){
-    //         return res.render('votar', { title: Elecciones, error: "No se pudo registrar al votante..."})
-    //     }
-    //     res.redirect('/');
-    // });
-    
-    // var filterDni = { dni: req.body.dni}
-    // Votante.find(filterDni, function(err, votantes){
-    //     if (err != null){
-    //         res.status(400).send(new Error("Error: " + err.message));
-    //     }
-    //     if (votantes != null){
-    //         res.status(400).send(new Error("Ya se ha votado con el número de DNI ingresado"));
-    //     }
-    //     else{
-    //         votante.save(function (err, response){
-    //             if(err){
-    //                 return res.render('/', { title: 'Elecciones' });
-    //             }
-    //             if(response == null){
-    //                 return res.render('/', { title: 'Elecciones', error: "No se pudo registrar sus datos..."});
-    //             }
-    //             res.redirect('/seleccionarCandidato');
-    //         });
-    //     }
-    // });
 });
 
 module.exports = router;

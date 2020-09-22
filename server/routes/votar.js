1b616275b889b2b4b8e477b59c080806dc7b5d12
Marina Carrizo <marinancarrizo@gmail.com>
2020-09-21 21:07:17 -0300

    Fixed votar/elegirCandidato/<some_dni>
    Fixed votar/elegirCandidato/<some_dni> without fill the form in votar.ejs
    Some cleaning in elegirCandidato.ejs and votar.js

--- server/routes/votar.js
+++ server/routes/votar.js
@@ -33,15 +33,21 @@ router.post('/', function(req, res, next){
                         return res.render('votar', { title: 'Elecciones' , error: err.message});
                     }
                     if (response == null){
-                        return res.render('votar', { title: Elecciones, error: "No se pudo registrar al votante..."})
+                        return res.render('votar', { title: 'Elecciones', error: "No se pudo registrar al votante..."})
                     }
-                    res.redirect('/votar/elegirCandidato/' + dni); 
-                    //res.render('/elegirCandidato', { title: "Elecciones" , dni: dni });
+                    res.redirect('/votar/elegirCandidato/' + dni);
                 });
             }
             else {
+                Votante.update({_id: response._id}, { nombre:req.body.nombre, apellido: req.body.apellido, sexo: req.body.sexo }, function (err, res){
+                    if(err) {
+                        return res.render('votar', { title: 'Elecciones', error: err.message});
+                    }
+                    if(res == null){
+                        return res.render('votar', { title: 'Elecciones', error: "No se pudo registrar al votante..."});
+                    }
+                });
                 res.redirect('/votar/elegirCandidato/' + dni);
-                //res.render('/elegirCandidato', { title: "Elecciones" , dni: dni });
             }
         }
     }));
@@ -63,25 +69,28 @@ router.get('/elegirCandidato/:dni', function(req, res, next){
 
 /* POST elegir candidato y registrar voto */
 router.post('/elegirCandidato', function (req, res, next){
-    var votante = new Votante({
-        dni: req.body.dni
-    });
-    var candidato = new Candidato({
-        _id: req.body._id
-    });
+    // var votante = new Votante({
+    //     dni: req.body.dni
+    // });
+    // var candidato = new Candidato({
+    //     _id: req.body._id
+    // });
     Votante.findOne({ dni: req.body.dni }, function(err, response){
         if (err) {
-            next(err);
-            // res.render('elegirCandidato', { title: 'Elecciones', error: err.message });
-            //res.redirect('/votar/elegirCandidato/' + dni, );
+            //next(err);
+            res.render('votar', { title: 'Elecciones', error: err.message });
         }
-        if (response != null && response.estadoVoto == true) {
-            res.render('votar', { title: 'Elecciones', error: 'Ya se ha votado con el DNI ingresado. ' });
+        if (response == null){
+            res.render('votar', { title: 'Elecciones', error: "Primero tiene que ingresar sus datos para poder votar"} );
+        }
+        else if (response != null && response.estadoVoto == true) {
+            res.redirect('votar', { title: 'Elecciones', error: 'Ya se ha votado con el DNI ingresado. ' });
         }
         else {
             Candidato.findById(req.body._id, function (err, rescandidato){
                 if (err) {
-                    next(err)
+                    res.render('votar', { title: 'Elecciones', error: err.message });
+                    //next(err)
                 }
                 if (rescandidato == null) {
                     res.render('votar', { title: 'Elecciones', error: 'No existe el candidato ' });
@@ -89,7 +98,8 @@ router.post('/elegirCandidato', function (req, res, next){
                 else{
                     Votante.update({_id: response._id}, { estadoVoto: true }, function (err,res){
                         if(err){
-                            next(err);
+                            res.render('votar', { title: 'Elecciones', error: err.message });
+                            //next(err);
                         }
                         if(res==null){
                             res.render('votar', { title: 'Elecciones', error: 'DNI incorrecto' });
@@ -97,7 +107,8 @@ router.post('/elegirCandidato', function (req, res, next){
                     });
                     Candidato.update({_id: rescandidato._id}, { cantidadVotos: rescandidato.cantidadVotos + 1 }, function(err, res){
                         if(err){
-                           next(err);
+                            res.render('votar', { title: 'Elecciones', error: err.message });
+                           //next(err);
                         }
                         if(res==null){
                             res.render('votar', { title: 'Elecciones', error: 'No existe el candidato' });
